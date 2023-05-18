7894ebffac75bf27772a5e4e43ee292bcb3e7dec
AKV85 <a.kotov.laknojus@gmail.com>
2023-05-18 13:01:44 +0300

    update success and error messages

--- my-app/src/components/ContactForm.jsx
+++ my-app/src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../styles/ContactForm.css';
 
 function ContactForm() {
@@ -7,30 +7,40 @@ function ContactForm() {
     const [message, setMessage] = useState('');
     const [errors, setErrors] = useState({});
     const [successMessage, setSuccessMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setIsLoading(true);
         try {
             const response = await fetch('http://localhost:81/api/contacts', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({name, email, message}),
+                body: JSON.stringify({ name, email, message }),
             });
             const data = await response.json();
             if (response.ok) {
-                console.log(data);
                 setName('');
                 setEmail('');
                 setMessage('');
                 setErrors({});
                 setSuccessMessage('Message sent successfully!');
+                setTimeout(() => {
+                    setSuccessMessage('');
+                }, 4000);
             } else {
                 setErrors(data.errors);
             }
+            setIsLoading(false);
         } catch (error) {
             console.error(error);
+            setErrors({ general: 'Failed to send message. Please try again later.' });
+            setTimeout(() => {
+                setErrors({});
+            }, 4000);
+            setIsLoading(false);
         }
     };
 
@@ -38,14 +48,15 @@ function ContactForm() {
         <form onSubmit={handleSubmit} className="contact-form">
             <h2>Contact Me</h2>
             {successMessage && <div className="success-message">{successMessage}</div>}
+            {errors.general && <div className="error-message">{errors.general}</div>}
             <div className="form-group">
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)}/>
+                <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)} />
                 {errors.name && <div className="error-message">{errors.name[0]}</div>}
             </div>
             <div className="form-group">
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} />
                 {errors.email && <div className="error-message">{errors.email[0]}</div>}
             </div>
             <div className="form-group">
@@ -53,7 +64,9 @@ function ContactForm() {
                 <textarea id="message" value={message} onChange={(event) => setMessage(event.target.value)}></textarea>
                 {errors.message && <div className="error-message">{errors.message[0]}</div>}
             </div>
-            <button type="submit">Submit</button>
+            <button type="submit" disabled={isLoading}>
+                {isLoading ? 'Loading...' : 'Submit'}
+            </button>
         </form>
     );
 }
