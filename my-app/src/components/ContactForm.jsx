6c069c1f740937cbc9a7f8316e1caa723a888cb8
AKV85 <a.kotov.laknojus@gmail.com>
2023-05-18 15:13:27 +0300

    update ip address

--- my-app/src/components/ContactForm.jsx
+++ my-app/src/components/ContactForm.jsx
@@ -13,7 +13,7 @@ function ContactForm() {
         event.preventDefault();
         setIsLoading(true);
         try {
-            const response = await fetch('http://172.21.45.88:81/api/contacts', {
+            const response = await fetch('http://172.21.44.100:81/api/contacts', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
