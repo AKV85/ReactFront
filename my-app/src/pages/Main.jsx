d1279b9c4e65be9d38dbf90dcefd4d1592fc3d54
AKV85 <a.kotov.laknojus@gmail.com>
2023-05-18 11:35:37 +0300

    update text on main page

--- my-app/src/pages/Main.jsx
+++ my-app/src/pages/Main.jsx
@@ -5,12 +5,12 @@ function Main() {
     return (
         <div className="main">
             <h2>Main Page</h2>
-            <p>This website is created with Netlify app (for primary information and developer
+            <p>This website is deployed using Netlify(for primary information and developer
                 contacts). </p>
             <p>Due to this, the functionality of sending messages and subscribing to news is not
                 available.</p>
             <p>The website was created using React on the frontend, Laravel API on the backend, and hosted using
-                AWS.</p>
+                Netlify.</p>
         </div>
     )
 }
