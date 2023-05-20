c28f7f92b00e5ee6e76f6b945f6c99c8b5cc6e4c
AKV85 <a.kotov.laknojus@gmail.com>
2023-05-20 20:49:50 +0300

    Update text in main.jsx

--- my-app/src/pages/Main.jsx
+++ my-app/src/pages/Main.jsx
@@ -5,12 +5,13 @@ function Main() {
     return (
         <div className="main">
             <h2>Main Page</h2>
-            <p>This website is deployed using Netlify(for primary information and developer
-                contacts). </p>
-            <p>Due to this, the functionality of sending messages and subscribing to news is not
-                available.</p>
-            <p>The website was created using React on the frontend, Laravel API on the backend, and hosted using
-                Netlify.</p>
+            <p>On the homepage of my project, I used a Laravel API back-end deployed on the Laravel Forge platform (forge.laravel.com). To accomplish this, I acquired a domain from Hostinger (hostinger.com) and configured DNS records for my API.</p>
+
+            <p>The front-end of my project is developed using React. For hosting the front-end, I utilized the Netlify platform (app.netlify.com) and obtained a domain through their platform.</p>
+
+            <p>Thus, I achieved by the perfect combination of a powerful Laravel API back-end deployed on Forge and accessible via a domain registered with Hostinger, along with a modern and responsive React front-end hosted on Netlify and accessible via a domain acquired on their platform.</p>
+
+            <p>Users can enjoy the reliability and speed of our Laravel API back-end provided by the Forge platform, as well as the instant loading and responsiveness of our React front-end facilitated by Netlify.</p>
         </div>
     )
 }
